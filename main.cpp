0bb3c0160159c4b531a76074ec5b93e79e253c16
theevilroot <robot-creatix@ya.ru>
2020-04-22 23:24:18 +0300

    work in progress

--- main.cpp
+++ main.cpp
@@ -8,65 +8,89 @@
 #include <aio.h>
 #include <fcntl.h>
 #include <cerrno>
+#include <memory>
 
 typedef struct aiocb aiocb_t;
 
 struct req_t {
-  int index;
+  std::size_t index;
   int status;
   char *buffer;
-  aiocb_t *op;
+  aiocb *op;
+  pthread_mutex_t *mutex;
 };
 
-static std::string gBuffer;
-
 void asyncReady(int, siginfo_t *info, void *) {
-  printf("info: %p\n", info);
-  printf("code: %d\n", info->si_code);
-  printf("is async: %d\n", info->si_code == SI_ASYNCIO);
-  printf("status: %d\n", info->si_status);
-  printf("signo: %d\n", info->si_signo);
-  printf("errno: %d\n", info->si_errno);
-  printf("value: %d\n", info->si_value);
-  printf("value ptr: %p\n", info->si_value.sival_ptr);
+  if (info->si_code == SI_ASYNCIO) {
+    if (info->si_value.sival_ptr == nullptr) {
+      printf("[T] sigval_ptr is nullptr\n");
+      return;
+    }
+    auto req = static_cast<req_t *>(info->si_value.sival_ptr);
+    printf("nbytes: %zu\n", req->op->aio_nbytes);
+    printf("return: %ld\n", aio_return(req->op));
+    for (int i = 0; i < req->op->aio_nbytes; i++)
+      printf("%02x ", static_cast<int>(req->buffer[i]));
+    printf("\n");
+    pthread_mutex_unlock(req->mutex);
+  }
 }
 
 void asyncFunction(sigval val) {
   printf("async function ptr: %p\n", val.sival_ptr);
 }
 
-req_t * asyncRead(const char *filename, size_t length, int sigNo) {
-  auto *op = (aiocb_t*) calloc(1, sizeof(aiocb_t));
-  auto *req = (req_t*) calloc(1, sizeof(req_t));
+req_t * asyncRead(const char *filename) {
+  pthread_mutex_t mutex { };
+  pthread_mutex_init(&mutex, nullptr);
 
-  req->buffer = (char*) calloc(length, sizeof(char));
+  auto fd = open(filename, O_RDONLY);
+  auto signo = SIGUSR1;
+  auto length = 4u;
+  auto off = 0u;
 
-  op->aio_fildes = open(filename, O_RDONLY);
-  op->aio_offset = 0;
-  op->aio_nbytes = length;
-  op->aio_buf = req->buffer;
-  op->aio_lio_opcode = LIO_READ;
+  while (true) {
+    auto *op = (aiocb_t *) calloc(1, sizeof(aiocb_t));
+    auto *req = (req_t *) calloc(1, sizeof(req_t));
 
-  op->aio_sigevent.sigev_notify = SIGEV_SIGNAL;
-  op->aio_sigevent.sigev_signo = sigNo;
-  op->aio_sigevent.sigev_value.sival_int = 1223;
-  op->aio_sigevent.sigev_notify_function = &asyncFunction;
+    req->mutex = &mutex;
+    req->buffer = (char *) calloc(length, sizeof(char));
 
-  req->op = op;
-  req->status = EINPROGRESS;
+    op->aio_fildes = fd;
+    op->aio_offset = off;
+    op->aio_nbytes = length;
+    op->aio_buf = req->buffer;
+    op->aio_lio_opcode = LIO_READ;
 
-  if (aio_read(op) == -1)
-    exit(1123);
+    op->aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+    op->aio_sigevent.sigev_signo = signo;
+    op->aio_sigevent.sigev_value.sival_ptr = req;
+
+    req->op = op;
+    req->status = EINPROGRESS;
+
+    pthread_mutex_lock(&mutex);
+    if (aio_read(op) == -1) {
+      pthread_mutex_unlock(&mutex);
+      exit(1123);
+    }
+  }
+
   return req;
 }
 
 int main() {
+  pthread_mutex_init(&mutex, nullptr);
   struct sigaction sa { };
   sa.sa_flags = SA_SIGINFO;
   sa.sa_sigaction = &asyncReady;
   if (sigaction(SIGUSR1, &sa, nullptr) == -1)
     printf("failed sigaction\n");
-  asyncRead("/dev/stdin", 4, SIGUSR1);
-  while (true) getchar();
+  auto file = open("/home/theevilroot/file", O_RDONLY);
+  auto length = 6u;
+  for (int i = 0; i < 8; i++) {
+    asyncRead(file, i * length, length, SIGUSR1);
+  }
+  while (true) usleep(10000000);
 }
 
