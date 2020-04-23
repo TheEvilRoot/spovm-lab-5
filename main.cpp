a2677277dd5d5c2ccb5b4faa4c866249ce250c51
theevilroot <robot-creatix@ya.ru>
2020-04-24 00:21:31 +0300

    working in single thread. (why do we need more???)

--- main.cpp
+++ main.cpp
@@ -38,16 +38,27 @@ struct buffer_t {
 };
 
 struct req_t {
+  int ofd;
   char *buffer;
   buffer_t *global_buffer;
   aiocb *op;
+  aiocb *oop;
   pthread_mutex_t *work_mutex;
   pthread_mutex_t *done_mutex;
 
   ~req_t() {
     free(buffer);
     free(op);
+    free(oop);
   }
+};
+
+struct wreq_t {
+  aiocb_t *op;
+  req_t *read_req;
+  int read_status;
+  int read_length;
+
 };
 
 template<typename A>
@@ -56,13 +67,14 @@ auto create_struct() {
   return new (mem) A;
 }
 
-req_t *read_file_async(int fd, int sig_no, int length, int offset, buffer_t *buffer, pthread_mutex_t *done_mutex, pthread_mutex_t *work_mutex) {
+req_t *read_file_async(int fd, int ofd, int sig_no, int length, int offset, buffer_t *buffer, pthread_mutex_t *done_mutex, pthread_mutex_t *work_mutex) {
   auto *op = create_struct<aiocb_t>();
   auto *req = create_struct<req_t>();
 
   req->buffer = static_cast<char*>(calloc(length + 1, sizeof(char)));
   req->done_mutex = done_mutex;
   req->work_mutex = work_mutex;
+  req->ofd = ofd;
 
   op->aio_fildes = fd;
   op->aio_offset = offset;
@@ -85,6 +97,30 @@ req_t *read_file_async(int fd, int sig_no, int length, int offset, buffer_t *buf
   return req;
 }
 
+void write_file_async(int fd, int sig_no, int length, int read_length, req_t *req) {
+  auto *wreq = create_struct<wreq_t>();
+  auto *op = create_struct<aiocb_t>();
+
+  wreq->op = op;
+  wreq->read_req = req;
+  wreq->read_status = length;
+  wreq->read_length = read_length;
+
+  op->aio_buf = req->buffer;
+  op->aio_fildes = req->ofd;
+  op->aio_nbytes = length;
+  op->aio_lio_opcode = LIO_WRITE;
+
+  op->aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+  op->aio_sigevent.sigev_signo = sig_no;
+  op->aio_sigevent.sigev_value.sival_ptr = wreq;
+
+  if (aio_write(op) == -1) {
+    delete op;
+    return;
+  }
+}
+
 void async_handler(int, siginfo_t *info, void*) {
   if (info->si_code == SI_ASYNCIO) {
     if (info->si_value.sival_ptr == nullptr) {
@@ -92,22 +128,41 @@ void async_handler(int, siginfo_t *info, void*) {
       return;
     }
     auto *req = static_cast<req_t *>(info->si_value.sival_ptr);
+
     auto ret_status = aio_return(req->op);
     auto n_bytes = req->op->aio_nbytes;
     auto offset = req->op->aio_offset;
 
     req->global_buffer->put(offset, req->buffer, n_bytes);
-
-    if (ret_status < n_bytes) {
-      printf("ret_status < n_bytes | %ld < %lu\n", ret_status, n_bytes);
-      printf("unlocking done mutex\n");
+    if (ret_status == 0) {
+      printf("ret_status = 0. skipping write. unlocking done mutex\n");
+      pthread_mutex_unlock(req->work_mutex);
       pthread_mutex_unlock(req->done_mutex);
+      return;
     }
-    pthread_mutex_unlock(req->work_mutex);
+
+    write_file_async(req->ofd, SIGUSR2, ret_status, n_bytes, req);
   }
 }
 
-buffer_t read_file(const char *filename) {
+void async_write_handler(int, siginfo_t *info, void*) {
+  if (info->si_code == SI_ASYNCIO) {
+    if (info->si_value.sival_ptr == nullptr) {
+      printf("[SW] sigval_ptr is nullptr");
+      return;
+    }
+    auto *wreq = static_cast<wreq_t *>(info->si_value.sival_ptr);
+
+    pthread_mutex_unlock(wreq->read_req->work_mutex);
+    if (wreq->read_status < wreq->read_length) {
+      printf("ret_status < n_bytes | %d < %d\n", wreq->read_status, wreq->read_length);
+      printf("unlocking done mutex\n");
+      pthread_mutex_unlock(wreq->read_req->done_mutex);
+    }
+  }
+}
+
+buffer_t read_file(const char *filename, int ofd) {
   auto length = 4u;
   buffer_t buffer(length);
 
@@ -115,8 +170,17 @@ buffer_t read_file(const char *filename) {
   sa.sa_flags = SA_SIGINFO;
   sa.sa_sigaction = &async_handler;
 
+  struct sigaction wsa { };
+  wsa.sa_flags = SA_SIGINFO;
+  wsa.sa_sigaction = &async_write_handler;
+
   if (sigaction(SIGUSR1, &sa, nullptr) == -1) {
     printf("failed sigaction\n");
+    return buffer;
+  }
+
+  if (sigaction(SIGUSR2, &wsa, nullptr) == -1) {
+    printf("failed wsa sigaction\n");
     return buffer;
   }
 
@@ -142,7 +206,7 @@ buffer_t read_file(const char *filename) {
       pthread_mutex_lock(&work_mutex);
       if (pthread_mutex_trylock(&done_mutex) != EBUSY)
         break;
-      auto req = read_file_async(fd, sig_no, length, off, &buffer, &done_mutex, &work_mutex);
+      auto req = read_file_async(fd, ofd, sig_no, length, off, &buffer, &done_mutex, &work_mutex);
       requests.push_back(req);
       off += length;
     }
@@ -162,8 +226,10 @@ buffer_t read_file(const char *filename) {
 }
 
 int main() {
-  auto buffer = read_file("/home/theevilroot/file");
+  int ofd = open("/home/theevilroot/output_file", O_WRONLY | O_CREAT | O_APPEND);
+  auto buffer = read_file("/home/theevilroot/file", ofd);
   std::cout << "'" << buffer.get_string() << "'\n";
+  close(ofd);
   return 0;
 }
 
